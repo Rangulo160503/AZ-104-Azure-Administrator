0bed3a63c8a79f49e26b335b3c7bb303703dfbd2
Ronald Angulo <105762511+Rangulo160503@users.noreply.github.com>
2026-09-11 22:18:20 -0600

    Complete AZ-104 Issue 1 practical activities

---
+++ ISSUE1/New Folder 2/az104.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+    std::cout << "AZ-104\n";
+    std::cout << "Identity &Governance\n";
+    std::cout << "20-25%\n";
+    std::cout << "Storage\n";
+    std::cout << "15-20%\n";
+    std::cout << "Compute\n";
+    std::cout << "20-25%\n";
+    std::cout << "Networking\n";
+    std::cout << "15-20%\n";
+    std::cout << "Monitoring\n";
+    std::cout << "10-15%\n";
+    std::cout << "Tenant\n";
+    std::cout << "  Management Groups\n";
+    std::cout << "    Subscriptions\n";
+    std::cout << "      Resource Groups\n";
+    std::cout << "        Resources\n";
+    std::cout << "          Virtual Machine\n";
+    std::cout << "          Storage Account\n";
+    std::cout << "          Virtual Network\n";
+    std::cout << "          Public IP\n";
+    std::cout << "          Network Security Group\n";
+}
---
+++ ISSUE1/New Folder 6/az104.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+int main() {
+    std::cout << "AZ-104\n";
+    std::cout << "Identity &Governance\n";
+    std::cout << "20-25%\n";
+    std::cout << "Storage\n";
+    std::cout << "15-20%\n";
+    std::cout << "Compute\n";
+    std::cout << "20-25%\n";
+    std::cout << "Networking\n";
+    std::cout << "15-20%\n";
+    std::cout << "Monitoring\n";
+    std::cout << "10-15%\n";
+    std::cout << "Tenant\n";
+    std::cout << "  Management Groups\n";
+    std::cout << "    Subscriptions\n";
+    std::cout << "      Resource Groups\n";
+    std::cout << "        Resources\n";
+    std::cout << "          Virtual Machine\n";
+    std::cout << "          Storage Account\n";
+    std::cout << "          Virtual Network\n";
+    std::cout << "          Public IP\n";
+    std::cout << "          Network Security Group\n";
+    std::cout << "\nAplicacion empresarial\n";
+    std::cout << "Usuarios y permisos -> Identity\n";
+    std::cout << "Ejecucion de la aplicacion -> Compute\n";
+    std::cout << "Almacenamiento -> Storage\n";
+    std::cout << "Conectividad -> Networking\n";
+    std::cout << "Supervision -> Monitoring\n";
+    std::cout << "\nVirtual Machine\n";
+    std::cout << "Planear\n";
+    std::cout << "Crear\n";
+    std::cout << "Configurar\n";
+    std::cout << "Proteger\n";
+    std::cout << "Supervisar\n";
+    std::cout << "Mantener\n";
+    std::cout << "Modificar / Escalar\n";
+    std::cout << "Eliminar cuando corresponda\n";
+    std::cout << "\nControl Plane\n";
+    std::cout << "Crear una Storage Account\n";
+    std::cout << "Crear/modificar un recurso\n";
+    std::cout << "\nData Plane\n";
+    std::cout << "Subir un archivo a Blob Storage\n";
+}
